30d902d4cd131db8c4d91fe60b1a2a7046cf7f2d
soszaboss <moustoifa10@outlook.fr>
2024-08-01 15:43:36 +0000

    update config.py

--- app/config.py
+++ app/config.py
@@ -5,14 +5,14 @@ import dotenv
 dotenv.load_dotenv()
 
 class Config:
-    SECRET_KEY = "02b374365cb8418b972b269a9a18982f"
-    JWT_SECRET_KEY = "4a6810c54e6d4ab59a6a3be0b3fbce9b"
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
     DATABASE = os.environ.get('CONNINFO')
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=60)
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS').lower() in ['true', '1', 't']
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL').lower() in ['true', '1', 't']
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL')
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
